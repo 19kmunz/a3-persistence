bb745dd40855b578762766aa61479598c917019a
Glitch (snowy-cedar-argon) <none>
2021-09-18 23:07:20 +0000

    🚏👨 Checkpoint ./public/css/style.css:35394881/112 ./public/js/scripts.js:35394881/2090

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,76 @@
 // Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+
+  const refreshGalleryContents = function(json) {
+    let galleryContents = '';
+    for(let i = 0; i < json.length; i++){
+      galleryContents += '<li> <figure> <img class="petTile" src="'+json[i].link+'" alt="Cute picture of '+json[i].name+'"> <figcaption>'+json[i].name+' says '+json[i].call+'</figcaption> </figure> <button id="'+json[i].id+'" onclick="deleteEntry(this.id)" class="delete">Delete</button> </li>';
+    }
+    const gallery = document.querySelector( '#gallery' )
+    gallery.innerHTML = galleryContents;
+  }
+  const deleteEntry = function (clickedId) {
+    body = JSON.stringify({"id": clickedId});
+    fetch( '/delete', {
+      method:'POST',
+      body
+    })
+    .then( function( response ) {
+      return response.json();
+    })
+    .then( function(json) {
+      refreshGalleryContents(json)
+    })
+  }
+  const submit = function( e ) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+    const name = document.querySelector( 'input[name="name"]' ),
+            link = document.querySelector( 'input[name="link"]' ),
+            type = document.querySelector( 'select[name="type"]' ),
+          json = { name: name.value, link: link.value, type: type.value },
+          body = JSON.stringify( json )
+
+    fetch( '/submit', {
+      method:'POST',
+      body 
+    })
+    .then( function( response ) {
+      return response.json();
+    })
+    .then( function(json) {
+      refreshGalleryContents(json)
+    })
+
+    return false
+  }
+
+  const submitNoFields = function( e ) {
+
+    const json = { name: '', link: '', type: '' },
+            body = JSON.stringify( json )
+
+    fetch( '/submit', {
+      method:'POST',
+      body
+    })
+    .then( function( response ) {
+      return response.json();
+    })
+    .then( function(json) {
+      refreshGalleryContents(json)
+    })
+
+    return false
+  }
+
+  window.onload = function() {
+    const button = document.querySelector( '#createPet' )
+    button.onclick = submit
+    submitNoFields()
+  }
+
+  window.onpageshow = function () {
+    submitNoFields()
+  }
