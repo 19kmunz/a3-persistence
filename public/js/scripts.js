419fd72e0452fbe2845d042a440b9c3fb5461754
Glitch (snowy-cedar-argon) <none>
2021-09-19 00:19:21 +0000

    😚🎸 Checkpoint ./public/index.html:35394881/42 ./public/js/scripts.js:35394881/708

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,7 +5,8 @@ const refreshGalleryContents = function(json) {
     galleryContents += '<li id="'
       +json[i].id+'"> <figure> <img class="petTile" src="'
       +json[i].link+'" alt="Cute picture of '
-      +json[i].name+'"> <figcaption>'
+      +json[i].name+'"> <figcaption name="'
+      +json[i].name+'">'
       +json[i].name+' says '
       +json[i].call+'</figcaption> </figure> <button value="'
       +json[i].id+'" onclick="deleteEntry(this.value)">Delete</button> <button value="'
@@ -71,22 +72,14 @@ const submitNoFields = function( e ) {
 }
 
 const enterEditMode = function(clickedId) {
-  const liElement = document.querySelector( "id="+clickedId )
-  <form action="">
-          <label>Pet Name: <input name="name" value="Moredcai"></label>
-          <label>Image Link: <input name="link" value="link"></label>
-          <label>Pet Type:
-            <select name="type" value="Cat">
-              <option>Dog</option>
-              <option>Cat</option>
-              <option>Snake</option>
-              <option>Bird</option>
-              <option>Other</option>
-            </select>
-          </label>
-        </form>
-        <button value="2" onclick="confirmEdits(this.value)">Edit That Pet!</button>
-        <button value="2" onclick="exitEditMode(this.value)">Cancel</button>
+  const liElement = document.querySelector( 'li[id="'+clickedId+'"]' )
+  const name = liElement.querySelector("figcaption").getAttribute("name")
+  const link = liElement.querySelector("img").getAttribute("src")
+  liElement.innerHTML = '<form action=""> <label>Pet Name: <input name="name" value="'
+    +name+'"></label> <label>Image Link: <input name="link" value="'
+    +link+'"></label><label>Pet Type:<select name="type" value="Cat"><option>Dog</option><option>Cat</option><option>Snake</option><option>Bird</option><option>Other</option></select></label></form><button value="'
+    +clickedId+'" onclick="confirmEdits(this.value)">Edit That Pet!</button><button value="'
+    +clickedId+'" onclick="exitEditMode(this.value)">Cancel</button>'
 }
 const exitEditMode = function(clickedId) {
   const liElement = document.querySelector( "id="+clickedId )
